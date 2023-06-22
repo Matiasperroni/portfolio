6c6128e5ac51d9953865c1b967fd15343a2f3e13
Matias Perroni <matiperroni.96@gmail.com>
2023-06-22 05:15:32 -0300

    hamburguer menu

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { HashLink } from "react-router-hash-link";
 import styles from "./nav.module.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
 
 const NavBar = () => {
+    const [burgerClass, setburgerClass] = useState(`${styles.burguerBar} ${styles.unclicked}`)
+    const [menuClass, setmenuClass] = useState(`${styles.containerNavLinks} ${styles.hidden}`)
+    const [isMenuClicked, setisMenuClicked] = useState(false)
+    console.log(menuClass);
+    const updateMenu = () => {
+        if(!isMenuClicked) {
+            setburgerClass(`${styles.burguerBar} ${styles.clicked}`)
+            setmenuClass(`${styles.containerNavLinks} ${styles.visible}`)
+            console.log("asd");
+        } else {
+            setburgerClass(`${styles.burguerBar} ${styles.unclicked}`)
+            setmenuClass(`${styles.containerNavLinks} ${styles.hidden}`)
+            console.log("asd");
+        }
+        setisMenuClicked(!isMenuClicked)
+    }
     return (
         <div className={styles.container}>
+            <div className={styles.burgerMenu} onClick={updateMenu}>
+                <div className={burgerClass}></div>
+                <div className={burgerClass}></div>
+                <div className={burgerClass}></div>
+            </div>
             <div className={styles.titleNav}>
                 <h1>Portfolio</h1>
             </div>
-            <div className={styles.containerNavLinks}>
+            
+            <div className={menuClass}>
                 <HashLink className={styles.navLink} to="#Home" smooth>
                     <p>Home</p>
                 </HashLink>
